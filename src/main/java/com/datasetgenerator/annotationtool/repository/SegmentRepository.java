dc3ce077c87fdf361dbc24fc8623df6d6b382de6
feriel kraiem <[feriel.benkraiem@ensi-uma.tn]>
2023-07-21 14:50:18 +0100

    adding getDetailForEachFile method

--- src/main/java/com/datasetgenerator/annotationtool/repository/SegmentRepository.java
+++ src/main/java/com/datasetgenerator/annotationtool/repository/SegmentRepository.java
@@ -2,6 +2,13 @@ package com.datasetgenerator.annotationtool.repository;
 
 import com.datasetgenerator.annotationtool.model.Segment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SegmentRepository extends JpaRepository<Segment,Long> {
+   @Query("SELECT COUNT(s.segment_id), AVG(s.segment_end - s.segment_start), SUM(s.segment_end -s.segment_start) FROM Segment s JOIN s.file f where f.file_id= :file_id")
+    List<Object[]> getDetailsForEachFile(@Param("file_id") Long file_id);
+
 }
