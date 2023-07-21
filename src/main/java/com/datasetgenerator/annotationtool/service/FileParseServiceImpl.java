f76cd0e8430cf50974cf046772e5b1c74f55616c
feriel kraiem <[feriel.benkraiem@ensi-uma.tn]>
2023-07-21 14:33:11 +0100

    refactor fileParsingAlgorithm

--- src/main/java/com/datasetgenerator/annotationtool/service/FileParseServiceImpl.java
+++ src/main/java/com/datasetgenerator/annotationtool/service/FileParseServiceImpl.java
@@ -5,6 +5,8 @@ import com.datasetgenerator.annotationtool.model.File;
 import com.datasetgenerator.annotationtool.model.Segment;
 import com.datasetgenerator.annotationtool.repository.FileRepository;
 import com.datasetgenerator.annotationtool.repository.SegmentRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,25 +20,20 @@ public class FileParseServiceImpl implements FileParseService {
     private final FileExtractContentService fileExtractContentService;
     private final FileRepository fileRepository;
     private final SegmentRepository segmentRepository;
+    @Value("${file.extensions}")
+    private String fileExtensions;
+    private int minimumFields;
 
-    public FileParseServiceImpl(FileExtractContentService fileExtractContentService, FileRepository fileRepository, SegmentRepository segmentRepository) {
+    public FileParseServiceImpl(FileExtractContentService fileExtractContentService, FileRepository fileRepository, SegmentRepository segmentRepository, @Value("${minimumFields}") int minimumFields) {
         this.fileExtractContentService = fileExtractContentService;
         this.fileRepository = fileRepository;
         this.segmentRepository = segmentRepository;
+        this.minimumFields = minimumFields;
     }
 
     public ResponseEntity<List<String>> extractValidLines(MultipartFile file) throws IOException {
         ResponseEntity<String> response = fileExtractContentService.readFileContent(file);
         String fileContent = response.getBody();
-        String minimumFieldsEnv = System.getenv("MINIMUM_FIELDS");
-        int minimumFields = 7;
-        if (minimumFieldsEnv != null) {
-            try {
-                minimumFields = Integer.parseInt(minimumFieldsEnv);
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-            }
-        }
         List<String> validLines = new ArrayList<>();
         String[] lines = fileContent.split("\n");
         for (String line : lines) {
@@ -45,44 +42,23 @@ public class FileParseServiceImpl implements FileParseService {
                 validLines.add(line);
             }
         }
+        System.out.println(validLines);
         return ResponseEntity.ok().body(validLines);
     }
 
-    public ResponseEntity<String> extractFileName(Map<String, String> fields) throws IOException {
 
-
-            String extensionsEnv = System.getenv("EXTENSIONS");
-            List<String> extensions = Arrays.asList(".wav", ".mp3");
-            if (extensionsEnv != null && !extensionsEnv.isEmpty()) {
-                extensions = Arrays.asList(extensionsEnv.split(","));
-            }
-            String fileName = "";
-            for (Map.Entry<String, String> entry : fields.entrySet()) {
-                String value = entry.getValue();
-                for (String extension : extensions) {
-                    if (value.endsWith(extension)) {
-                        int extensionIndex = value.lastIndexOf(extension);
-                        fileName = value.substring(0, extensionIndex+extension.length());
-                        break;
-                    }
-                }
-            }
-            return ResponseEntity.ok(fileName);
-        }
-
-    public ResponseEntity<List<Map<String, String>>> extractLineContent(MultipartFile file) throws IOException {
+    public ResponseEntity<List<List<String>>> extractLineContent(MultipartFile file) throws IOException {
 
         ResponseEntity<List<String>> responseEntity = extractValidLines(file);
         List<String> validLines = responseEntity.getBody();
-        List<Map<String, String>> lineContent = new ArrayList<>();
+        List<List<String>> lineContent = new ArrayList<>();
         assert validLines != null;
         for (String line : validLines) {
             String[] fields = line.split(" ");
-            Map<String, String> currentLineContent = new LinkedHashMap<>();
+            List<String> currentLineContent = new ArrayList<>();
             for (int i = 0; i < fields.length; i++) {
-
                 if (!fields[i].equals("1")) {
-                    currentLineContent.put("field" + i, fields[i]);
+                    currentLineContent.add(fields[i]);
                 }
 
             }
@@ -91,52 +67,97 @@ public class FileParseServiceImpl implements FileParseService {
         return ResponseEntity.ok(lineContent);
     }
 
-    public ResponseEntity<List<Map<String, String>>> extractFields(MultipartFile file) throws IOException {
+    public ResponseEntity<String> extractFileName(List<String> fields) throws IOException {
+        List<String> extensions = Arrays.asList(fileExtensions.split(","));
+        String fileName = "";
+        for (String field : fields) {
+            for (String extension : extensions) {
+                if (field.endsWith(extension)) {
+                    int extensionIndex = field.lastIndexOf(extension);
+                    fileName = field.substring(0, extensionIndex + extension.length());
+                    break;
+                }
+            }
+        }
 
+        return ResponseEntity.ok(fileName);
+    }
+
+    public ResponseEntity<StringBuilder> extractTranscription(List<String> fields) {
+        StringBuilder transcription = new StringBuilder();
+        boolean afterField4 = false;
+        for (String field : fields) {
+            if (afterField4) {
+                transcription.append(field).append(" ");
+            }
+            if (field.equals(fields.get(4))) {
+                afterField4 = true;
+            }
+        }
+        return ResponseEntity.ok(transcription);
+    }
+
+    public ResponseEntity<List<Map<String, String>>> extractFields(MultipartFile file) throws IOException {
         List<String> validLines = extractValidLines(file).getBody();
-        ResponseEntity<List<Map<String, String>>> lineContentResponse = extractLineContent(file);
-        List<Map<String, String>> lineContent = lineContentResponse.getBody();
+        ResponseEntity<List<List<String>>> lineContentResponse = extractLineContent(file);
+        List<List<String>> lineContent = lineContentResponse.getBody();
         List<Map<String, String>> outputLines = new ArrayList<>();
         File fileEntity = new File();
         fileRepository.save(fileEntity);
+        boolean fileExists = false;
+        String fileName = "";
         for (int i = 0; i < Objects.requireNonNull(validLines).size(); i++) {
             assert lineContent != null;
-            Map<String, String> fields = lineContent.get(i);
+            List<String> fields = lineContent.get(i);
             Map<String, String> outputLine = new LinkedHashMap<>();
-            String fileName = extractFileName(fields).getBody();
+            fileName = extractFileName(fields).getBody();
             outputLine.put("file_name", fileName);
-            outputLine.put("speaker", fields.get("field2"));
-            outputLine.put("segment_start", fields.get("field3"));
-            outputLine.put("segment_end", fields.get("field4"));
-            StringBuilder transcription = new StringBuilder();
-            boolean afterField5 = false;
-
-            for (Map.Entry<String, String> entry : fields.entrySet()) {
-                if (afterField5) {
-                    transcription.append(entry.getValue()).append(" ");
-                }
-                if (entry.getKey().equals("field5")) {
-                    afterField5 = true;
-                }
-            }
+            outputLine.put("speaker", fields.get(1));
+            outputLine.put("segment_start", fields.get(2));
+            outputLine.put("segment_end", fields.get(3));
+            double segmentStart = Double.parseDouble(outputLine.get("segment_start"));
+            double segmentEnd = Double.parseDouble(outputLine.get("segment_end"));
+            double duration = segmentEnd - segmentStart;
+            StringBuilder transcription = extractTranscription(fields).getBody();
             outputLine.put("transcription", String.valueOf(transcription));
             outputLines.add(outputLine);
-
+            if (fileRepository.existsByFileName(fileName)) {
+                fileExists = true;
+                Map<String, String> errorResponse = new LinkedHashMap<>();
+                errorResponse.put("error", "File already exists!");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.singletonList(errorResponse));
+            }
             Segment segment = new Segment();
-            segment.setFile_name(fileName);
             segment.setSpeaker(outputLine.get("speaker"));
             segment.setSegment_start(Double.parseDouble(outputLine.get("segment_start")));
             segment.setSegment_end(Double.parseDouble(outputLine.get("segment_end")));
+            segment.setDuration(duration);
             segment.setTranscription(outputLine.get("transcription"));
             segment.setFile(fileEntity);
+            segment.setDuration(duration);
             segmentRepository.save(segment);
-            fileEntity.getSegments().add(segment);
         }
-
-        fileRepository.save(fileEntity);
+        if (!fileExists) {
+            fileEntity.setFile_name(fileName);
+            fileRepository.save(fileEntity);
+        }
 
         return ResponseEntity.ok(outputLines);
     }
 
+    public ResponseEntity<Map<String, Object>> getDetailsForEachFile(Long file_id) {
+        List<Object[]> result = segmentRepository.getDetailsForEachFile(file_id);
+        Map<String, Object> fileDetails = new LinkedHashMap<>();
+        for (Object[] row : result) {
+
+            Long segmentCount = (Long) row[0];
+            Double averageDuration = (Double) row[1];
+            Double totalDuration = (Double) row[2];
+            fileDetails.put("segmentCount", segmentCount);
+            fileDetails.put("averageDuration", averageDuration);
+            fileDetails.put("totalDuration", totalDuration);
+        }
+        return ResponseEntity.ok(fileDetails);
+    }
 }
 
