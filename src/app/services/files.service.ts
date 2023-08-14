235abdd2d39c7217141d9a487d406e8b31c6894e
feriel.benkraiem@ensi-uma.tn <feriel.benkraiem@ensi-uma.tn>
2023-08-14 02:03:26 +0100

    fix:unused-import

--- src/app/services/files.service.ts
+++ src/app/services/files.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Statistics } from '../interfaces/statistics.interface'
-import { TimeConverterPipe } from '../pipes/time-converter.pipe';
 @Injectable({
   providedIn: 'root'
 })
