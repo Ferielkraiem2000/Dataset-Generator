9f021823011fadd3ac7adb904000fbfdaf10eef0
feriel.benkraiem@ensi-uma.tn <feriel.benkraiem@ensi-uma.tn>
2023-08-23 15:15:03 +0100

    fi:file-content-design

--- src/app/components/content-file/content-file.component.ts
+++ src/app/components/content-file/content-file.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommunicationService } from 'src/app/services/communication.service';
 
 @Component({
   selector: 'app-content-file',
   templateUrl: './content-file.component.html',
   styleUrls: ['./content-file.component.css'],
-  providers:[CommunicationService]
 })
-export class ContentFileComponent {
+export class ContentFileComponent implements OnInit{
   constructor(private communicationService:CommunicationService){}
+  content:any[]=[];
+  isContent(){
+    return this.communicationService.content;
+  }
+ ngOnInit(): void {
+   this.content=this.isContent();
+ }
 }
